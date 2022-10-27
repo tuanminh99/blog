6a132491c828f569cd173b40f56041f4361f0bac
tuanminh <minhcola9999@gmail.com>
2022-10-27 16:23:08 +0700

    express handlebars

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars');
+const app = express()
+const port = 3000
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars.engine({
+  extname: ".hbs"
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views')
+// console.log('PATH: ', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
