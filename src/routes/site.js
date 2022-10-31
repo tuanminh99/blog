725abe7d2250c40a2105afda45e10f428038ba95
tuanminh <minhcola9999@gmail.com>
2022-10-31 16:11:20 +0700

    add prettier + lint-staged + husky

--- src/routes/site.js
+++ src/routes/site.js
@@ -6,4 +6,4 @@ const siteControllers = require('../app/controllers/SiteControllers');
 router.use('/search', siteControllers.search);
 router.use('/', siteControllers.home);
 
-        module.exports = router;
+            module.exports = router;
