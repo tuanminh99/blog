40a8e8bda68fcc0b95124097b5f74e540be648e2
tuanminh <minhcola9999@gmail.com>
2022-11-01 14:28:27 +0700

    MVC Model

--- src/app/controllers/SiteControllers.js
+++ src/app/controllers/SiteControllers.js
@@ -1,7 +1,16 @@
+const Course = require('../models/Course');
+
 class SiteControllers {
     // [GET] /home
     home(req, res) {
-        res.render('home');
+        // res.render('home');
+        Course.find({}, function (err, courses) {
+            if (!err) {
+                res.json(courses);
+            } else {
+                res.status(400).json({ error: 'ERROR!!' });
+            }
+        });
     }
     // [GET] /search
     search(req, res) {
